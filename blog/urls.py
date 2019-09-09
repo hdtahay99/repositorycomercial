a65931368bbd880fc513ac0f69e164725e490113
Diosymar Menchú <hdtahay@gmail.com>
2019-09-09 13:02:23 -0600

    Nueva actualización sobre crud

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.listar_pub, name='listar_pub'),
+    path('pub/<int:pk>/', views.detalle_pub, name='detalle_pub'),
+    path('pub/nuevo/', views.nuevo_pub, name='nuevo_pub'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,28 @@ from django.utils import timezone
 
 from django.shortcuts import render
 from .models import Publicacion
+from django.shortcuts import render, get_object_or_404
+from .forms import FormPub
+from django.shortcuts import redirect
 # Create your views here.
 
 def listar_pub(request):
     pubs = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar_pub.html', {'pubs' : pubs})
+
+def detalle_pub(request, pk):
+    pub = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/detalle_pub.html', {'pub': pub})
+
+def nuevo_pub(request):
+    if request.method == "POST":
+        form = FormPub(request.POST)
+        if form.is_valid():
+            pub = form.save(commit=False)
+            pub.autor = request.user
+            pub.fecha_publicacion = timezone.now()
+            pub.save()
+            return redirect('detalle_pub', pk=pub.pk)
+    else:
+        form = FormPub()
+    return render(request, 'blog/nuevo_pub.html', {'formulario': form})
